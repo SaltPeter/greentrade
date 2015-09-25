d913aeabf8a6824c158309c658bf9274c6a9242a
SaltPeter <peter.tran3@carleton.ca>
2015-09-25 08:30:49 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,21 @@
 <?php
 @include_once("settings.php");
 
+$username = $_REQUEST['username']; 
+$password = $_REQUEST['password']; 
+
+$check = mysql_query("SELECT * FROM `users` WHERE `username`='" . $username . "'" ) or die (mysql_error());
+$numrows = mysql_num_rows($check);
+if ($numrows == 0)
+	die("Username doesn't exist \n");
+else {
+	$password = password_hash($password, PASSWORD_BCRYPT);
+	while ($row = mysql_fetch_assoc($check)) {
+		if($password == $row['password'])
+			die("userid=".$row['userid']);
+		else
+			die("Incorrect login");
+	}
+}
+
 ?>
