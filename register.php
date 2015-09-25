f34fd420655a829f89816e0e6379b7caf5692c1c
SaltPeter <peter.tran3@carleton.ca>
2015-09-25 08:23:30 -0400

    Update register.php

--- register.php
+++ register.php
@@ -13,4 +13,21 @@ else
 
 $password = password_hash($password, PASSWORD_BCRYPT);
 
+if(mysql_query("SELECT `username` FROM `users` where `username`=`" . $username. "`"))
+	mysql_query("INSERT INTO `users` VALUES ({$username}', '{$password}', '{$email}', '(10,000)')") or die ('Query failed: ' . mysql_error());
+
+$findid = mysql_query("SELECT `userid` FROM `users` WHERE `username`='" . $username . "'" ) or die (mysql_error());
+
+$numrows = mysql_num_rows($findid);
+
+$message = "";
+if ($numrows == 0)
+	die(" the new user not found \n");
+else {
+	while($row = mysql_fetch_assoc($findid))
+		$userid = $row['id'] ;  break;
+			
+echo "done, userid:".$userid."";
+}
+
 ?>
